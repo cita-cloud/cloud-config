d5e8a31200f85838ecde6b5863b7ca4deb43e41e
Zhishi <whfuyn@outlook.com>
2021-12-17 16:42:53 +0800

    load kms type from chain config

--- src/import_account.rs
+++ src/import_account.rs
@@ -17,19 +17,16 @@ use std::path::Path;
 
 use clap::Clap;
 
+use anyhow::bail;
 use anyhow::Context;
 use anyhow::Result;
-use anyhow::bail;
 
 use crate::{
+    config::{kms_eth::KmsEth, kms_sm::KmsSm},
+    constant::{ACCOUNT_DIR, CHAIN_CONFIG_FILE, KEY_ID, KMS_DB, KMS_ETH, KMS_SM},
     traits::Kms,
-    util::write_file,
-    config::{
-        kms_eth::KmsEth,
-        kms_sm::KmsSm,
-    },
+    util::{find_micro_service, read_chain_config, write_file},
 };
-
 
 /// A subcommand for import account, only kms_sm is supported
 #[derive(Clap, Debug, Clone)]
@@ -40,9 +37,6 @@ pub struct ImportAccountOpts {
     /// set config file directory, default means current directory
     #[clap(long = "config-dir", default_value = ".")]
     pub(crate) config_dir: String,
-    /// kms type
-    #[clap(long = "kms-type", default_value = "kms_sm")]
-    pub(crate) kms_type: String,
     /// kms db password
     #[clap(long = "kms-password", default_value = "123456")]
     pub(crate) kms_password: String,
@@ -68,23 +62,32 @@ fn import_account<K: Kms, P: AsRef<Path>>(
     };
     fs::create_dir_all(&account_dir).context("cannot create account dir")?;
 
-    let db_path = account_dir.join("kms.db").to_string_lossy().into();
+    let db_path = account_dir.join(KMS_DB).to_string_lossy().into();
     let kms = K::create_kms_db(db_path, kms_password.into());
     let (key_id, addr) = kms.import_privkey(&privkey);
 
-    let key_id_path = account_dir.join("key_id");
+    let key_id_path = account_dir.join(KEY_ID);
     write_file(key_id.to_string().as_bytes(), key_id_path);
 
     Ok((key_id, addr))
 }
 
 pub fn execute_import_account(opts: ImportAccountOpts) -> Result<(u64, String)> {
-    let base_dir = format!("{}/{}/accounts", &opts.config_dir, &opts.chain_name);
+    // load chain_config
+    let file_name = format!(
+        "{}/{}/{}",
+        &opts.config_dir, &opts.chain_name, CHAIN_CONFIG_FILE
+    );
+    let chain_config = read_chain_config(&file_name).unwrap();
+
+    let base_dir = format!("{}/{}/{}", &opts.config_dir, &opts.chain_name, ACCOUNT_DIR);
     let (key_id, addr) = {
-        let import_account = match opts.kms_type.as_str() {
-            "kms_sm" => import_account::<KmsSm, _>,
-            "kms_eth" => import_account::<KmsEth, _>,
-            _ => bail!("unknown kms type"),
+        let import_account = if find_micro_service(&chain_config, KMS_ETH) {
+            import_account::<KmsEth, _>
+        } else if find_micro_service(&chain_config, KMS_SM) {
+            import_account::<KmsSm, _>
+        } else {
+            bail!("unknown kms type");
         };
         import_account(base_dir, &opts.kms_password, &opts.privkey)
             .context("cannot import account")?
