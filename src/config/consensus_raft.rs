3bc1c4ef9e45bca1e587bf79fac60968a5974e84
zhiwei <rink1969@gmail.com>
2022-01-28 15:07:43 +0800

    add license and copyright info (#14)

--- src/config/consensus_raft.rs
+++ src/config/consensus_raft.rs
@@ -11,6 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 use crate::constant::CONSENSUS_RAFT;
 use crate::traits::{TomlWriter, YmlWriter};
 use serde::{Deserialize, Serialize};
--- src/config/kms_sm.rs
+++ src/config/kms_sm.rs
@@ -11,6 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 use crate::constant::{KMS, KMS_SM};
 use crate::traits::{TomlWriter, YmlWriter};
 use serde::{Deserialize, Serialize};
